4dff9147d7f958ac6dd63b4f780fe7187567a63a
yrathore9911 <yrathore9911@gmail.com>
2024-06-01 11:06:03 +0530

    Updating jenkins file

--- main.tf
+++ main.tf
@@ -77,3 +77,10 @@ resource "aws_route_table" "public" {
     Name = "public-route-table"
   }
 }
+
+# Route table associate
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
