ca89aef02bb59dfa5e9d4b5f31b24653ded13f51
yrathore9911 <yrathore9911@gmail.com>
2024-06-01 10:53:21 +0530

    Updating jenkins file

--- main.tf
+++ main.tf
@@ -28,3 +28,27 @@ resource "aws_subnet" "private" {
     Name = "private-subnet"
   }
 }
+
+# Creating Security group
+
+resource "aws_security_group" "allow_ssh" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
