53c1a99499299d76c85f0aaaf5ed5b169e5fcca9
yrathore9911 <yrathore9911@gmail.com>
2024-06-01 13:59:14 +0530

    Updating jenkins file

--- main.tf
+++ main.tf
@@ -140,3 +140,29 @@ resource "aws_nat_gateway" "natgw" {
   allocation_id = aws_eip.natgwip.id
   subnet_id     = aws_subnet.public.id
 }
+
+# Creating route table for nat gateway.
+
+
+resource "aws_route_table" "private-rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.natgw.id
+  }
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+# Route table associate for nat gateway
+
+resource "aws_route_table_association" "private-asso" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private-rt.id
+}
+
+
+
