08c6d3539b52e74f6b23ea045942599397572f5d
yrathore9911 <yrathore9911@gmail.com>
2024-06-01 10:40:16 +0530

    Updating jenkins file

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-resource "aws_instance" "first-ec2" {
-  count         = 0
-  ami           = "ami-00fa32593b478ad6e"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "testing"
-  }
-}
 # Creating VPC
 
 resource "aws_vpc" "main" {
