43b866d3f8bfbcce1c6d2636befa8efd766515bf
yrathore9911 <yrathore9911@gmail.com>
2024-06-01 12:23:50 +0530

    Updating jenkins file

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ resource "aws_key_pair" "newkey" {
   }
 
 
-# Creating Public subnet
+# Creating Public instance
 
 resource "aws_instance" "public_instance" {
   ami           = "ami-00fa32593b478ad6e"  # Update with a valid AMI ID for your region
@@ -107,4 +107,15 @@ resource "aws_instance" "public_instance" {
   }
 }
 
+# Creating Private instance
 
+resource "aws_instance" "private_instance" {
+  ami           = "ami-00fa32593b478ad6e"  # Update with a valid AMI ID for your region
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private.id
+  security_groups = [aws_security_group.allow_ssh.id]
+  key_name      = "newkey"
+
+  tags = {
+    Name = "private-instance"
+}
