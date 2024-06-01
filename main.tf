bd192b6b151ba52f3af28935424e8ffd5b64ff93
yrathore9911 <yrathore9911@gmail.com>
2024-06-01 11:01:29 +0530

    Updating jenkins file

--- main.tf
+++ main.tf
@@ -62,3 +62,18 @@ resource "aws_internet_gateway" "gw" {
     Name = "main-gateway"
   }
 }
+
+# Creating route table
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
