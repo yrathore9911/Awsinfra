9c2b73b604cbe1c3d29a352cd6da2b784b5818e4
yrathore9911 <yrathore9911@gmail.com>
2024-06-01 10:12:18 +0530

    Updating jenkins file

--- main.tf
+++ main.tf
@@ -123,25 +123,3 @@ resource "aws_instance" "private_instance" {
     Name = "private-instance"
   }
 }
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "public_subnet_id" {
-  value = aws_subnet.public.id
-}
-
-output "private_subnet_id" {
-  value = aws_subnet.private.id
-}
-
-output "public_instance_id" {
-  value = aws_instance.public_instance.id
-}
-
-output "private_instance_id" {
-  value = aws_instance.private_instance.id
-}
-output "public_instance_ip" {
-  value = aws_instance.public.*.public_ip
-}
